854639288c98b0721391777a5da9b7815168d898
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-18 16:36:31 -0700

    Removing another erroneous reference to 'this' in onConnection callback

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.15',
+  version: '1.2.16',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
