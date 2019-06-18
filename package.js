328e36d40b26075bd9b54ba88915bacfc14043c0
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-18 16:41:04 -0700

    Adding console.log of Meteor.settings.public.userMetaData

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.16',
+  version: '1.2.17',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
