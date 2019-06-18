fef161e4e4510782b5fb1356e7484a58ee1e5646
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-18 16:17:40 -0700

    Removing references to 'this' in the onConnection callback

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.13',
+  version: '1.2.14',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
