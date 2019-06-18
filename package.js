9a6ab49bdb3da14ede4b64c75526580ef1b0978c
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-18 12:55:05 -0700

    Adding support for logging user meta data

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.8',
+  version: '1.2.9',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
