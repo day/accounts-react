798b2d164701c12e92878b2420ae10501a98bae8
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-19 17:53:06 -0700

    Setting the session inside the getLocationData method

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.24',
+  version: '1.2.25',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
