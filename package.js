8a6d0a382bc7e0b7f11e75f312c5eef078fcec56
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-19 18:04:20 -0700

    Removing onConnection callback and referecing this.connection instead

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.25',
+  version: '1.2.26',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
