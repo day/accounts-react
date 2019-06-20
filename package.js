61147c55ec066dc11975149408fec80d87bf4f72
Day Davis Waterbury <day.waterbury@gmail.com>
2019-06-19 18:18:38 -0700

    Moving back to Meteor.settings.public (rather than session)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'day:accounts-react',
   summary: 'Meteor Accounts UI for React',
-  version: '1.2.26',
+  version: '1.2.27',
   documentation: 'README.md',
   git: 'https://github.com/day/accounts-react'
 })
